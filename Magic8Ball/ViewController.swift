199888466d7a49be4067726b2683671dd5be5d2f
Kingsley Esezobor <54145107+kingsohio@users.noreply.github.com>
2020-08-16 23:38:03 -0400

    Update ViewController.swift

--- Magic8Ball/ViewController.swift
+++ Magic8Ball/ViewController.swift
@@ -14,7 +14,38 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func ShakeButtonTapped(_ sender: Any) {
+        generateAnswer()
+        //print("Shake it like a polaroid picture")
+        
+        //answerLabel.text = "button was tapped"
+        
+        //RandomlygeneratebyButton
+        // 1
+        //let randomIndex = Int.random(in: 0..<["Yes, definitely", "It is certain", "Without a doubt", "Yes", "Most likely", "Sure, why not?", "Same", "Tell me more", "Out to lunch", "Reply hazy, try again", "Ask again later", "The cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no", "Absolutely not"].count)
 
+        // 2
+        //answerLabel.text = ["Yes, definitely", "It is certain", "Without a doubt", "Yes", "Most likely", "Sure, why not?", "Same", "Tell me more", "Out to lunch", "Reply hazy, try again", "Ask again later", "The cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no", "Absolutely not"][randomIndex]
+        
+        
+        
+    }
+    //RandomlygeneratebyMotion
+    override func motionEnded(_ motion : UIEvent.EventSubtype, with event: UIEvent?) {
+        guard motion == .motionShake else { return }
+        
+        generateAnswer()
+        //let randomIndex = Int.random(in: 0..<["Yes, definitely", "It is certain", "Without a doubt", "Yes", "Most likely", "Sure, why not?", "Same", "Tell me more", "Out to lunch", "Reply hazy, try again", "Ask again later", "The cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no", "Absolutely not"].count)
+        
+        //answerLabel.text = ["Yes, definitely", "It is certain", "Without a doubt", "Yes", "Most likely", "Sure, why not?", "Same", "Tell me more", "Out to lunch", "Reply hazy, try again", "Ask again later", "The cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no", "Absolutely not"][randomIndex]
+        
+        
+    }
+    //generateAnswer method - both Shake and button
+    func generateAnswer() {
+        let randomIndex = Int.random(in: 0..<["Yes, definitely", "It is certain", "Without a doubt", "Yes", "Most likely", "Sure, why not?", "Same", "Tell me more", "Out to lunch", "Reply hazy, try again", "Ask again later", "The cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no", "Absolutely not"].count)
+        
+        answerLabel.text = ["Yes, definitely", "It is certain", "Without a doubt", "Yes", "Most likely", "Sure, why not?", "Same", "Tell me more", "Out to lunch", "Reply hazy, try again", "Ask again later", "The cake is a lie", "42", "TMI", "Very doubtful", "Don't count on it", "My reply is no", "Absolutely not"][randomIndex]
 
 }
 
